3899344924b608b77c8b9fe87c281b5853267d68
Ljcs1999 <Ljcs_1999@hotmail.com>
2022-12-02 22:43:16 -0800

    Intalacion de zorro

---
+++ src/app/api/services.ts
@@ -0,0 +1,8 @@
+export { CargoControllerService } from './services/cargo-controller.service';
+export { CargoEmpleadoControllerService } from './services/cargo-empleado-controller.service';
+export { CodigoTrabajoControllerService } from './services/codigo-trabajo-controller.service';
+export { EmpleadoControllerService } from './services/empleado-controller.service';
+export { EmpleadoCargoControllerService } from './services/empleado-cargo-controller.service';
+export { EmpleadoVacacionesControllerService } from './services/empleado-vacaciones-controller.service';
+export { VacacionesControllerService } from './services/vacaciones-controller.service';
+export { VacacionesEmpleadoControllerService } from './services/vacaciones-empleado-controller.service';
